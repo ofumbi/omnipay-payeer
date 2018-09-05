108186f22f503a6e59c0f2f07cf54618db629584
ofumbi <ofuzak@gmail.com>
2018-09-05 21:11:45 +0300

    Update to Omnipay 3.0
    Signed-off-by: ofumbi <ofuzak@gmail.com>

--- src/Message/RefundRequest.php
+++ src/Message/RefundRequest.php
@@ -25,10 +25,9 @@ class RefundRequest extends AbstractRequest
 
     public function sendData($data)
     {
-        $httpResponse = $this->httpClient->post($this->endpoint, null, $data)->send();
-        $jsonResponse = json_decode($httpResponse->getBody(true));
-
-        return $this->response = new RefundResponse($this, $jsonResponse);
+        $response = $this->httpClient->request('POST', $this->endpoint, [], http_build_query($data));
+ 		$jsonResponse = json_decode($httpResponse->getBody()->getContents());
+		return $this->response = new RefundResponse($this, $jsonResponse);
     }
 
 }
